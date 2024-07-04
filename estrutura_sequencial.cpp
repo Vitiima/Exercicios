9770bae88ebfe5ef4b625feb84dd15c011eb75e8
Vitor Matheus Almeida <matheus_famas@hotmail.com>
2024-07-04 09:27:49 -0300

    .

---
+++ estrutura_sequencial.cpp
@@ -0,0 +1,18 @@
+
+#include "estrutura_sequencial.h"
+
+namespace estrutura_sequencial  {
+} // estrutura_sequencial
+
+estrutura_sequencial::estrutura_sequencial() {
+
+}
+
+float exer1(float n1, float n2, float n3, float n4) {
+    return n1+n2+n3+n4;
+}
+
+float exer2(float n1, float n2, float n3) {
+    float sum = n1+n2+n3;
+    return sum/3;
+}
---
+++ estrutura_sequencial.h
@@ -0,0 +1,24 @@
+
+#ifndef ESTRUTURA_SEQUENCIAL_H
+#define ESTRUTURA_SEQUENCIAL_H
+
+namespace estrutura_sequencial  {
+
+class estrutura_sequencial {
+public:
+    estrutura_sequencial() {
+    }
+    ~estrutura_sequencial() {
+
+    }
+    float exer1(){
+    }
+    float exer2(){
+    }
+
+private:
+};
+
+} // estrutura_sequencial 
+
+#endif //ESTRUTURA_SEQUENCIAL_H
